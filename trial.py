4f08af825d03ece9dbd3d299a72794cffb6bda83
M.Ramautar <144722341+MarisaR12@users.noreply.github.com>
2024-12-07 21:16:26 -0500

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,56 +1,57 @@
+#CFU17
+#Marisa Ramautar
+#12/6/24
+#Period 5-6
+#scaling code for scaling images
+
 import simplegui
-import math
-# Global Variables
+width = int(input("Inital Width?"))
+height = int(input("Inital Height?"))
 
-width = int(input("Enter a width value: "))
-height = int(input("Enter a Height value: "))
-face1 = False
-face2 = False
-face3 = False
-face4 = False
-face5 = False
-scale= 0.4
-# Event Handlers
+Marisa = Hailey = False
+show_hailey = True
+show_marisa = True
+show_triangle = True
+show_ellipse = True
+scale = 0.4
 
+frame = None #Global reference to the frame
+    
+    
 def all_false():
-    global face1
-    global face2
-    global face3
-    global face4
-    global face5
-    face1 = False
-    face2 = False
-    face3 = False
-    face4 = False
-    face5 = False
-   
-def toggle_face1():
-    all_false()
-    global face1
-    face1 = not face1
-   
-def toggle_face2():
-    all_false()
-    global face2
-    face2 = not face2
-     
-def toggle_face3():
-    all_false()
-    global face3
-    face3 = not face3
-   
-def toggle_face4():
-    all_false()
-    global face4
-    face4 = not face4
+    global show_hailey
+    global show_marisa
+    global show_triangle
+    global show_ellipse
+    show_hailey = False
+    show_marisa = False
+    show_triangle = False
+    show_ellipse = False
 
-def toggle_face5():
+    
+def hailey_emoji():
     all_false()
-    global face5
-    face5 = not face5
-
+    global show_hailey
+    show_hailey = not show_hailey
+       
+def marisa_emoji():
+    all_false()
+    global show_marisa
+    show_marisa = not show_marisa
+    
+def show_triangle():
+    all_false()
+    global show_triangle
+    show_triangle = not show_triangle
+    
+def show_ellipse():
+    all_false()
+    global show_ellipse
+    show_ellipse = not show_ellipse
+    
 def draw(canvas):
-    if face1:
+    
+    if show_hailey:
         canvas.draw_circle(((width/2)/2, (height/2)/2), (width/2 - 50)*scale, 10, "Yellow", "Yellow")
        
         # Right eye (winking)
@@ -67,7 +68,7 @@ def draw(canvas):
      
 
        
-    if face2:
+    if marisa_emoji:
         canvas.draw_circle((width-(width/2)/2, (height/2)/2), (width/2 - 50)*scale, 10, "Yellow", "Yellow")
         canvas.draw_line([width/6,height/3],[width/3,height/3],5*scale,"white")
         canvas.draw_line([width-(width/6),height/3],[width-(width/3),height/3],5,"black")
@@ -79,7 +80,7 @@ def draw(canvas):
         
         
        
-    if face3:
+    if show_triangle:
         # Face
         canvas.draw_circle((width-(width/2)/2, (height/1.4)), (width/2 - 50)*scale, 10, "#FA8072", "#FA8072")
     # Eyes
@@ -91,12 +92,10 @@ def draw(canvas):
         canvas.draw_polygon([(width/4, height/3 + 50),(3 * width/4,height/3+50),(3 * width/4, 2 * height/3), (width/4, 2 * height/3)], 2,
         "DarkOrange", "DarkOrange")
         
+                
         
         
-        
-        
-        
-    if face4:
+    if show_ellipse:
         canvas.draw_circle(((width/2)/2, (height/1.4)), (width/2 - 50)*scale, 10, "#FA8072", "#FA8072")
         canvas.draw_circle((200, 200), 30, 2, "Black", "White")
         canvas.draw_circle((400, 200), 30, 2, "Black", "White")
@@ -110,15 +109,13 @@ def draw(canvas):
         canvas.draw_line((400, 150),(440, 180), 3, "Black")
         canvas.draw_circle((300, 400), 30, 2, "Black", "Black")
 
-    if face5:
-        canvas.draw_circle((300, 300), 250, 10, "#FCF4A3", "#FCF4A3")
-frame = simplegui.create_frame("Pictures", width, height)
 
-frame.set_draw_handler(draw)
-frame.add_button("Hailey", toggle_face1, 100)
-frame.add_button("Marisa", toggle_face2, 100)
-frame.add_button("Malachi", toggle_face3, 100)
-frame.add_button("Farhan", toggle_face4, 100)
-frame.add_button("Everything", toggle_face5, 100)
+def create_frame():
+    global frame
+    frame = simplegui.create_frame("CFU #17",width,height)
+    frame.set_canvas_background("green")
+    frame.add_button("triangle", hailey_emoji,100)
+    frame.set_draw_handler(draw)
+    frame.start() #starts the frame
 
-frame.start()
+create_frame()
